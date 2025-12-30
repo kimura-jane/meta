6c200f74d938c7dcd1b3542a0eb35af58d07b2f9
木村 <lakokubunji@gmail.com>
2025-12-30 19:05:00 +0900

    Update room.js

--- party/room.js
+++ party/room.js
@@ -1,80 +1,80 @@
-export default class Room {
-  constructor(party) {
-    this.party = party;
-    this.users = {};
-  }
-
-  onConnect(conn, ctx) {
-    const userId = conn.id;
-    this.users[userId] = {
+export default {
+  async onConnect(connection, room) {
+    const userId = connection.id;
+    
+    // ユーザー情報を保存
+    const users = JSON.parse(await room.storage.get("users") || "{}");
+    users[userId] = {
       id: userId,
       x: (Math.random() - 0.5) * 8,
       y: 0.5,
-      z: 5 + Math.random() * 3,
-      color: this.getRandomColor()
+      z: 5 + Math.random() * 3
     };
+    await room.storage.put("users", JSON.stringify(users));
 
-    conn.send(JSON.stringify({
+    // 初期化メッセージ送信
+    connection.send(JSON.stringify({
       type: 'init',
-      users: this.users,
+      users: users,
       yourId: userId
     }));
 
-    this.party.broadcast(JSON.stringify({
+    // 他のユーザーに通知
+    room.broadcast(JSON.stringify({
       type: 'userJoin',
-      user: this.users[userId]
-    }), [conn.id]);
-  }
+      user: users[userId]
+    }), [connection.id]);
+  },
 
-  onMessage(message, sender) {
+  async onMessage(message, connection, room) {
     const data = JSON.parse(message);
+    const users = JSON.parse(await room.storage.get("users") || "{}");
 
     switch (data.type) {
       case 'position':
-        if (this.users[sender.id]) {
-          this.users[sender.id].x = data.x;
-          this.users[sender.id].y = data.y;
-          this.users[sender.id].z = data.z;
+        if (users[connection.id]) {
+          users[connection.id].x = data.x;
+          users[connection.id].y = data.y;
+          users[connection.id].z = data.z;
+          await room.storage.put("users", JSON.stringify(users));
         }
-        this.party.broadcast(JSON.stringify({
+        room.broadcast(JSON.stringify({
           type: 'position',
-          userId: sender.id,
+          userId: connection.id,
           x: data.x,
           y: data.y,
           z: data.z
-        }), [sender.id]);
+        }), [connection.id]);
         break;
 
       case 'reaction':
-        this.party.broadcast(JSON.stringify({
+        room.broadcast(JSON.stringify({
           type: 'reaction',
-          userId: sender.id,
+          userId: connection.id,
           reaction: data.reaction,
           color: data.color
-        }), [sender.id]);
+        }), [connection.id]);
         break;
 
       case 'chat':
-        this.party.broadcast(JSON.stringify({
+        room.broadcast(JSON.stringify({
           type: 'chat',
-          userId: sender.id,
+          userId: connection.id,
           name: data.name,
           message: data.message
         }));
         break;
     }
-  }
+  },
 
-  onClose(conn) {
-    delete this.users[conn.id];
-    this.party.broadcast(JSON.stringify({
+  async onClose(connection, room) {
+    const users = JSON.parse(await room.storage.get("users") || "{}");
+    delete users[connection.id];
+    await room.storage.put("users", JSON.stringify(users));
+    
+    room.broadcast(JSON.stringify({
       type: 'userLeave',
-      userId: conn.id
+      userId: connection.id
     }));
   }
-
-  getRandomColor() {
-    const colors = [0x4fc3f7, 0xff6b6b, 0x98d8aa, 0xffd93d, 0xc9b1ff, 0xff9ff3];
-    return colors[Math.floor(Math.random() * colors.length)];
-  }
-}
+};
